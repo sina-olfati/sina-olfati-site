c2037b679929d7a1f36f1f01fc91ac09129d9c7a
sina <sinaolfati5@gmail.com>
2022-07-24 18:32:46 +0430

    making a theme(failed)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import './App.css';
+import Index from './screens/Index';
+
+// Theme
+import theme from './theme/theme';
+import { ThemeProvider } from '@mui/material/styles';
+import {provider}
+import { CssBaseline } from '@mui/material';
+import rtlPlugin from 'stylis-plugin-rtl';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import { prefixer } from 'stylis';
+
+import { getDirection } from './localization/index';
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-     App
-    </div>
-  );
+  return getDirection() === "ltr" ? (
+    <ThemeProvider theme={theme}>
+      {/* <StylesProvider> */}
+        <CssBaseline />
+        <Index/>
+      {/* </StylesProvider> */}
+     </ThemeProvider>
+  ) : (
+    <ThemeProvider theme={theme}>
+      <StylesProvider>
+        <CssBaseline />
+        <Index/>
+      </StylesProvider>
+     </ThemeProvider>
+  )
 }
 
 export default App;
---
+++ src/theme/palette.ts
@@ -0,0 +1,13 @@
+export default {
+  primary:{
+    main: "#87E1C7",
+    contrastText: "#FFFFFF"
+  },
+  secondary:{
+    main: "#EAEBED"
+  },
+  text:{
+    primary:"#000000",
+    secondary:"#21605D"
+  }
+}
