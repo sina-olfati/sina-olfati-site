45f574f810e0ea92d74c9708bb05ca61a728f5ac
sina <sinaolfati5@gmail.com>
2022-07-25 17:23:41 +0430

    undo using theme(material ui)

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,14 @@ import React from 'react';
 import './App.css';
 import Index from './screens/Index';
 
-// Theme
-import theme from './theme/theme';
-import { ThemeProvider } from '@mui/material/styles';
-import {provider}
-import { CssBaseline } from '@mui/material';
-import rtlPlugin from 'stylis-plugin-rtl';
-import { CacheProvider } from '@emotion/react';
-import createCache from '@emotion/cache';
-import { prefixer } from 'stylis';
-
-import { getDirection } from './localization/index';
-
-
 
 
 function App() {
-  return getDirection() === "ltr" ? (
-    <ThemeProvider theme={theme}>
-      {/* <StylesProvider> */}
-        <CssBaseline />
+  return (
+    <div className='App'>
         <Index/>
-      {/* </StylesProvider> */}
-     </ThemeProvider>
-  ) : (
-    <ThemeProvider theme={theme}>
-      <StylesProvider>
-        <CssBaseline />
-        <Index/>
-      </StylesProvider>
-     </ThemeProvider>
+        Sina
+     </div>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-import { getDirection } from './localization/index';
-document.getElementsByTagName('body')[0].setAttribute('dir', getDirection())
-
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
