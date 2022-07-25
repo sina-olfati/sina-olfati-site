ec0d5641b853df38fdb7db9deba629dac85b6c13
sina <sinaolfati5@gmail.com>
2022-07-25 19:16:47 +0430

    making the site bilingual with i18next

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,24 @@ import React from 'react';
 import './App.css';
 import Index from './screens/Index';
 
+import { useTranslation } from "react-i18next";
+
+
 
 
 function App() {
+
+  const { t } = useTranslation()
+
   return (
-    <div className='App'>
+    <div className='container'>
         <Index/>
-        Sina
+        <div>
+          <a href="#" onClick={() => localStorage.setItem('i18nextLng', 'en')}>English</a>
+          /
+          <a href='#' onClick={() => localStorage.setItem('i18nextLng', 'fa')}>فارسی</a>
+        </div>
+        <h1>{t("welcome_message")}</h1>
      </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,33 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
+// i18next
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportedLngs: ["en", "fa"],
+    fallbackLng: "en",
+    detection: {
+      order: ['localStorage', 'htmlTag', 'cookie', 'path', 'subdomain'],
+      caches: ['cookie']
+    },
+    backend: {
+      loadPath: 'assets/locales/{{lng}}/translation.json',
+    },
+    react: {useSuspense: false},
+
+    interpolation: {
+      escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+    }
+  });
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
