12e2515feff20e0daf3b699729623f15cc85bdd7
sina <sinaolfati5@gmail.com>
2022-08-02 20:20:40 +0430

    Portfolio: made the cards better

--- src/screens/Portfolio.tsx
+++ src/screens/Portfolio.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './Portfolio.css'
 
+// images
+import mySite from'../assets/images/mySite.png'
+
 // Components
 import Menu from '../components/menu/Menu'
 
@@ -9,10 +12,11 @@ const Portfolio = () => {
     <div className='port-container'>
         <Menu />
         <div className='port-cards'>
+
           <div className='card'>
             <div className='card-picture'>
               <p>Sina's Site</p>
-              <img src='ss'/>
+              <img src={ mySite }/>
             </div>
             <div className='card-text'>
               <h1>Sina's Site</h1>
@@ -21,7 +25,20 @@ const Portfolio = () => {
               <a href='#'>Visit the site</a>
             </div>
           </div>
-          <div className='card'>Two</div>
+
+          <div className='card'>
+          <div className='card-picture'>
+              <p>Sina's Site</p>
+              <img src={ mySite }/>
+            </div>
+            <div className='card-text'>
+              <h1>Sina's Site</h1>
+              <h2>describtion</h2>
+              <h3>technologies</h3>
+              <a href='#'>Visit the site</a>
+            </div>
+          </div>
+
         </div>
     </div>
   )
