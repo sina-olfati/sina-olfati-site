4a6dcb2c0c306726fe0cc6e59036e4eac7bd0624
sina <sinaolfati5@gmail.com>
2022-08-02 19:39:34 +0430

    Portfolio: Made the cards

--- src/screens/Portfolio.tsx
+++ src/screens/Portfolio.tsx
@@ -8,7 +8,21 @@ const Portfolio = () => {
   return (
     <div className='port-container'>
         <Menu />
-      Portfolio
+        <div className='port-cards'>
+          <div className='card'>
+            <div className='card-picture'>
+              <p>Sina's Site</p>
+              <img src='ss'/>
+            </div>
+            <div className='card-text'>
+              <h1>Sina's Site</h1>
+              <h2>describtion</h2>
+              <h3>technologies</h3>
+              <a href='#'>Visit the site</a>
+            </div>
+          </div>
+          <div className='card'>Two</div>
+        </div>
     </div>
   )
 }
