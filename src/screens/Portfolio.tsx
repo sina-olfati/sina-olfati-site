3019ae451bd984b936c528ae37acec08294b4169
sina <sinaolfati5@gmail.com>
2022-08-03 11:51:30 +0430

    Portfolio: made it bilingual

--- src/screens/Portfolio.tsx
+++ src/screens/Portfolio.tsx
@@ -1,41 +1,56 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './Portfolio.css'
+import { Link } from 'react-router-dom'
 
 // images
 import mySite from'../assets/images/mySite.png'
 
 // Components
 import Menu from '../components/menu/Menu'
+import { t } from 'i18next'
+
+// i18next
+import { useTranslation } from "react-i18next";
 
 const Portfolio = () => {
+
+  const [language, setLanguage] = useState(localStorage.getItem("i18nextLng"))
+  // const isEn = bodyDir == "ltr" ? true : false
+
+  const { t } = useTranslation()
+
+  useEffect(() => {
+    setLanguage(localStorage.getItem("i18nextLng"))
+  }, [localStorage.getItem("i18nextLng")])
+
+
   return (
     <div className='port-container'>
         <Menu />
-        <div className='port-cards'>
+        <div className='port-cards' lang={language}>
 
           <div className='card'>
             <div className='card-picture'>
-              <p>Sina's Site</p>
+              <p>{t("portfolio-one-name")}</p>
               <img src={ mySite }/>
             </div>
             <div className='card-text'>
-              <h1>Sina's Site</h1>
-              <h2>describtion</h2>
-              <h3>technologies</h3>
-              <a href='#'>Visit the site</a>
+              <h1>{t("portfolio-one-name")}</h1>
+              <h2>{t("portfolio-one-description")}</h2>
+              <h3>{t("portfolio-one-technologies")}</h3>         
             </div>
           </div>
 
           <div className='card'>
           <div className='card-picture'>
-              <p>Sina's Site</p>
+              <p>{t("portfolio-two-name")}</p>
               <img src={ mySite }/>
             </div>
             <div className='card-text'>
-              <h1>Sina's Site</h1>
-              <h2>describtion</h2>
-              <h3>technologies</h3>
-              <a href='#'>Visit the site</a>
+              <h1>{t("portfolio-two-name")}</h1>
+              <h2>{t("portfolio-two-description")}</h2>
+              <h3>{t("portfolio-two-technologies")}</h3>
+              <Link to='/'>{t("portfolio-two-visit")}</Link>
             </div>
           </div>
 
