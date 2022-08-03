852ff080d2219f099e72295e0024b719f8d64ebe
sina <sinaolfati5@gmail.com>
2022-08-03 18:18:19 +0430

    Contact: adding formik(uncompleted)

--- src/components/contact/EmailMe.tsx
+++ src/components/contact/EmailMe.tsx
@@ -4,14 +4,21 @@ import './EmailMe.css'
 // Formik
 import { useFormik } from 'formik'
 
+
+
+interface MyValues {
+    name: string,
+    message: string
+}
+
 const EmailMe = () => {
 
-    const formik = useFormik({
+    const formik: MyValues = useFormik({
         initialValues: {
             name: '',
             message: ''
         }
-    })
+    });
 
 
 
