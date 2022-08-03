4b6a18b56ecd71e65db8f78f30e69f35312ebd74
sina <sinaolfati5@gmail.com>
2022-08-03 23:36:20 +0430

    Contact: added formik

--- src/components/contact/EmailMe.tsx
+++ src/components/contact/EmailMe.tsx
@@ -1,34 +1,50 @@
-import React from 'react'
+import * as React from 'react'
 import './EmailMe.css'
 
+// Material Ui
+import { Button } from '@mui/material';
+import {TextField} from '@mui/material';
+
 // Formik
-import { useFormik } from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup'
 
+const validationSchema = yup.object ({
+  name: yup.string().required('Please enter your name'),
+  message: yup.string().required('Please write a message'),
+})
 
-
-interface MyValues {
-    name: string,
-    message: string
-}
 
 const EmailMe = () => {
 
-    const formik: MyValues = useFormik({
-        initialValues: {
-            name: '',
-            message: ''
-        }
-    });
+    //     initialValues<MyValues>: {
+    //         name: '',
+    //         message: ''
+    //     }
 
 
 
 
   return (
     <div className='emailme-container'>
-      <form onSubmit={formik.handleSubmit}>
-        <h1>Email ME</h1>
-        <div></div>
-      </form>
+      <h1>Email Me</h1>
+      <Formik
+       initialValues={{ name: '', message: '' }}
+       onSubmit={(values) => { console.log(values) }}
+       validationSchema={validationSchema}
+      >
+        <Form className='form-tag'>
+          <div className='field-container'>
+            <Field placeholder='name' name='name' /> 
+            <ErrorMessage name='name' />
+          </div>
+          <div className='field-container'>
+            <Field placeholder="message" name='message' as='textarea'/>
+            <ErrorMessage name='message'/>
+          </div>
+          <Button type='submit'>SEND</Button>
+        </Form>
+      </Formik>
     </div>
   )
 }
