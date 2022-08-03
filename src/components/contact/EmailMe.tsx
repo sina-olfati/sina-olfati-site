2fb237d61a5bd54b7edb4df38f3d82e08a1f9f98
sina <sinaolfati5@gmail.com>
2022-08-03 18:00:55 +0430

    Contact: adding images | making the EmailMe file

---
+++ src/components/contact/EmailMe.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './EmailMe.css'
+
+// Formik
+import { useFormik } from 'formik'
+
+const EmailMe = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            message: ''
+        }
+    })
+
+
+
+
+  return (
+    <div className='emailme-container'>
+      <form onSubmit={formik.handleSubmit}>
+        <h1>Email ME</h1>
+        <div></div>
+      </form>
+    </div>
+  )
+}
+
+export default EmailMe
--- src/screens/Contact.tsx
+++ src/screens/Contact.tsx
@@ -4,8 +4,11 @@ import { Link } from 'react-router-dom'
 
 // Components
 import Menu from '../components/menu/Menu';
+import EmailMe from '../components/contact/EmailMe';
 
 // Images
+import links from  '../assets/images/links.jpg'
+import email from  '../assets/images/email.jpg'
 
 // Icons
 import EmailIcon from '@mui/icons-material/Email';
@@ -23,19 +26,21 @@ const Contact = () => {
         <div className='social-links'>
             <div>
                 <ul>
-                    <li><a href="mailto:sinaolfati6@gmail.com"><EmailIcon style={{fontSize: '2.2rem', position: "relative", top: "8px"}}/><span>sinaolfati6@gmail.com</span></a></li>
-                    <li><a href="tel:+989941669736"><PhonelinkRingIcon style={{fontSize: '2.2rem', position: "relative", top: "8px"}}/><span>+98994-166-9736</span></a></li>
-                    <li><a href="https://www.linkedin.com/in/sina-olfati-872950233"><LinkedInIcon style={{fontSize: '2.2rem', position: "relative", top: "8px"}}/><span>sina-olfati-872950233</span></a></li>
-                    <li><a href="https://github.com/sina-olfati"><GitHubIcon style={{fontSize: '2.2rem', position: "relative", top: "8px"}}/><span>sina-olfati</span></a></li>
-                    <li><a href="https://www.instagram.com/sina_._olfati"><InstagramIcon style={{fontSize: '2.2rem', position: "relative", top: "8px"}}/><span>sina_._olfati</span></a></li>
-                    <li><a><TelegramIcon style={{fontSize: '2.2rem', position: "relative", top: "8px"}}/><span>@sinaolfati</span></a></li>
+                    <li><a href="mailto:sinaolfati6@gmail.com"><EmailIcon style={{fontSize: '2.2rem', position: "relative", top: "11px"}}/><span>sinaolfati6@gmail.com</span></a></li>
+                    <li><a href="tel:+989941669736"><PhonelinkRingIcon style={{fontSize: '2.2rem', position: "relative", top: "11px"}}/><span>+98994-166-9736</span></a></li>
+                    <li><a href="https://www.linkedin.com/in/sina-olfati-872950233"><LinkedInIcon style={{fontSize: '2.2rem', position: "relative", top: "11px"}}/><span>sina-olfati-872950233</span></a></li>
+                    <li><a href="https://github.com/sina-olfati"><GitHubIcon style={{fontSize: '2.2rem', position: "relative", top: "11px"}}/><span>sina-olfati</span></a></li>
+                    <li><a href="https://www.instagram.com/sina_._olfati"><InstagramIcon style={{fontSize: '2.2rem', position: "relative", top: "11px"}}/><span>sina_._olfati</span></a></li>
+                    <li><a><TelegramIcon style={{fontSize: '2.2rem', position: "relative", top: "11px"}}/><span>@sinaolfati</span></a></li>
                 </ul>
             </div>
-            <img src='ss' />
+            <img src={ links } />
         </div>
         <div className='email-me'>
-            <img src='ss' />
-            <div></div>
+            <img src={ email } />
+            <div className='form-container'>
+                <EmailMe />
+            </div>
         </div>
         <div className='the-end'>
             <img src='ss' />
