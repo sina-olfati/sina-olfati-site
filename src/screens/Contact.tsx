e99c4e4f917806437153e325b3e158a7dfde4a67
sina <sinaolfati5@gmail.com>
2022-08-05 20:00:02 +0430

    Contact: improved the links

--- src/screens/Contact.tsx
+++ src/screens/Contact.tsx
@@ -14,6 +14,7 @@ import idea from '../assets/images/idea.jpg'
 // Icons
 import EmailIcon from '@mui/icons-material/Email';
 import PhonelinkRingIcon from '@mui/icons-material/PhonelinkRing';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import InstagramIcon from '@mui/icons-material/Instagram';
@@ -46,10 +47,11 @@ const Contact = () => {
                 <ul>
                     <li><a href="mailto:sinaolfati6@gmail.com"><EmailIcon style={{fontSize: '2.2rem', position: "relative", top: "11px"}}/><span dir='ltr'>sinaolfati6@gmail.com</span></a></li>
                     <li><a href="tel:+989941669736"><PhonelinkRingIcon style={{fontSize: '2.2rem', position: "relative", top: "11px"}}/><span dir='ltr'>+989941669736</span></a></li>
+                    <li><a href="https://api.whatsapp.com/send?phone=+989941669736"><WhatsAppIcon style={{fontSize: '2.2rem', position: "relative", top: "11px"}}/><span dir='ltr'>+989941669736</span></a></li>
+                    <li><a href="https://telegram.me/sinaolfati"><TelegramIcon style={{fontSize: '2.2rem', position: "relative", top: "11px"}}/><span dir='ltr'>@sinaolfati</span></a></li>
                     <li><a href="https://www.linkedin.com/in/sina-olfati-872950233"><LinkedInIcon style={{fontSize: '2.2rem', position: "relative", top: "11px"}}/><span dir='ltr'>sina-olfati-872950233</span></a></li>
                     <li><a href="https://github.com/sina-olfati"><GitHubIcon style={{fontSize: '2.2rem', position: "relative", top: "11px"}}/><span dir='ltr'>sina-olfati</span></a></li>
                     <li><a href="https://www.instagram.com/sina_._olfati"><InstagramIcon style={{fontSize: '2.2rem', position: "relative", top: "11px"}}/><span dir='ltr'>sina_._olfati</span></a></li>
-                    <li><a><TelegramIcon style={{fontSize: '2.2rem', position: "relative", top: "11px"}}/><span dir='ltr'>@sinaolfati</span></a></li>
                 </ul>
             </div>
             <img src={ links } />
@@ -62,7 +64,7 @@ const Contact = () => {
         </div>
         <div className='the-end'>
             <img src={idea} />
-            <h1>Im now learning... New updates will come</h1>
+            <h1>{t("contact-update")}</h1>
         </div>
         </div>
     </div>
