872ecebe586da7bcc3fb7c973f0996e7e4e8229f
sina <sinaolfati5@gmail.com>
2022-12-19 23:03:48 +0330

    Home: changed top text

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -33,7 +33,7 @@ const Home = () => {
       <div className='home-body'>
         <main>
           <section className="no-parallax">
-            <h1 className='top-text'>"{t("home_top")}"</h1>
+            <h1 className='top-text'>{t("home_top1")}<br />{t("home_top2")}</h1>
             <div className='scroll'>
               <h2>{t("home_scroll")}</h2>
               <div className='arrows'>
